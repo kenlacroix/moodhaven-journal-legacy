0fabcd3afa98de5defd5a7d56baa179a6accf581
Kenneth LaCroix <53909268+kenlacroix@users.noreply.github.com>
2026-03-26 12:27:55 -0600

    Delete website directory

--- website/app/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/page.tsx
-import { getSubstackPosts } from "@/lib/getSubstackPosts";
-import HomeClient from "@/components/HomeClient";
-
-export default async function Home() {
-  const posts = await getSubstackPosts();
-  return <HomeClient posts={posts} />;
-}
